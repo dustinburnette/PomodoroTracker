569581481faa836bf1a5d0669098e4c034737dce
Dustin Burnette <dustinburnette7861@hotmail.com>
2017-05-02 11:01:10 -0500

    Created Data Model for Pomodoro Tracker

---
+++ PomodoroTracker/Models/Pomodoro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PomodoroTracker.Models
+{
+    public class Pomodoro
+    {
+        public int PomodoroID { get; set; }
+
+        public int ProjectTaskID { get; set; }
+        public ProjectTask ProjectTask { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public int DurationInMinutes  { get; set; }
+    }
+}
---
+++ PomodoroTracker/Models/ProjectTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PomodoroTracker.Models
+{
+    public class ProjectTask
+    {
+        public int ProjectTaskID { get; set; }
+        public string Description { get; set; }
+        public int EstimatedPomodoroCount { get; set; }
+
+        public int ProjectID { get; set; }
+        public Project Project { get; set; }
+
+        public List<Pomodoro> Pomodoros { get; set; }
+    }
+}
