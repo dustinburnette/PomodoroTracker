f0b16d3249c14be985eab4bfb0ee84dff63e5230
Dustin Burnette <dustinburnette7861@hotmail.com>
2017-05-04 11:23:46 -0500

    can add projects on index page of projects with JS

---
+++ PomodoroTracker/Controllers/ProjectsApiController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using PomodoroTracker.Models;
+
+namespace PomodoroTracker.Controllers
+{
+    public class ProjectsApiController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/ProjectsApi
+        public IQueryable<Project> GetProjects()
+        {
+            return db.Projects;
+        }
+
+        // GET: api/ProjectsApi/5
+        [ResponseType(typeof(Project))]
+        public IHttpActionResult GetProject(int id)
+        {
+            Project project = db.Projects.Find(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(project);
+        }
+
+        // PUT: api/ProjectsApi/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProject(int id, Project project)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != project.ProjectID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(project).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProjectExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ProjectsApi
+        [ResponseType(typeof(Project))]
+        public IHttpActionResult PostProject(Project project)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Projects.Add(project);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = project.ProjectID }, project);
+        }
+
+        // DELETE: api/ProjectsApi/5
+        [ResponseType(typeof(Project))]
+        public IHttpActionResult DeleteProject(int id)
+        {
+            Project project = db.Projects.Find(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            db.Projects.Remove(project);
+            db.SaveChanges();
+
+            return Ok(project);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProjectExists(int id)
+        {
+            return db.Projects.Count(e => e.ProjectID == id) > 0;
+        }
+    }
+}
---
+++ PomodoroTracker/Scripts/App/ProjectsIndex.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    $('#NewProjectForm').on('submit', function (event) {
+        event.preventDefault();
+        //Get the value from #NewProjectDescriptionInput
+        var newProjectDescription = $('#NewProjectDescriptionInput').val();
+        //Post the new description to our API
+        $.post('/api/projectsapi', { Description: newProjectDescription })
+            .done(function (data) {
+                var newProjectID = data.ProjectID;
+                var newProjectEditLink = '<a href="/Projects/Edit/' + newProjectID + '">Edit</a>';
+                var newProjectDetailsLink = '<a href="/Projects/Details/' + newProjectID + '">Details</a>';
+                var newProjectDeleteLink = '<a href="/Projects/Edit/' + newProjectID + '">Delete</a>';
+
+                $('table tbody').append('<tr><td>' + newProjectDescription + '</td><td>' + newProjectEditLink + ' | ' + newProjectDetailsLink + ' | ' + newProjectDeleteLink + '</td></tr>');
+                $('#NewProjectDescriptionInput').val('');
+                console.log(data);
+
+            });
+    });
+
+});
